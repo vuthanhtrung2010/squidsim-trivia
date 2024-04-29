7fe69e2feb6d6d8285695487abeff444812c24b6
vuthanhtrung2010 <vuthanhtrungsuper@gmail.com>
2024-04-29 04:28:15 +0000

    fixes

--- handlers/loaddb.js
+++ handlers/loaddb.js
@@ -5,7 +5,7 @@ module.exports = async (client) => {
   return new Promise(async (res) => {
     let dateNow = Date.now();
     console.log(`${String("[x] :: ")}Now loading the Database ...`);
-    client.prisma = new PrismaClient().$extends(withAccelerate);
+    client.prisma = new PrismaClient().$extends(withAccelerate());
     console.log(
       `[x] :: ` + `LOADED THE DATABASE after: ` + `${Date.now() - dateNow}ms`
     );
